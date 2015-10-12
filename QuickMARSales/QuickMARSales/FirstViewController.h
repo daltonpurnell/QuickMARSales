6fd631a1870603eeaa925f7d5380fabfe62e4af2
daltonpurnell <daltonpurnell@live.com>
2015-10-12 10:05:13 -0600

    inital commit

---
+++ QuickMARSales/QuickMARSales/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  QuickMARSales
+//
+//  Created by Dalton on 10/12/15.
+//  Copyright © 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ QuickMARSales/QuickMARSales/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  QuickMARSales
+//
+//  Created by Dalton on 10/12/15.
+//  Copyright © 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
