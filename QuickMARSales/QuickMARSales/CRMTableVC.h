0e5889e0a6ad2e2c5e56c1259f8e14329a6961df
daltonpurnell <daltonpurnell@live.com>
2015-10-23 09:54:25 -0600

    adds crm tab

---
+++ QuickMARSales/QuickMARSales/CRMTableVC.h
@@ -0,0 +1,13 @@
+//
+//  CRMTableVC.h
+//  QuickMARSales
+//
+//  Created by Dalton on 10/23/15.
+//  Copyright © 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRMTableVC : UITableViewController
+
+@end
