8cd97ab876c6c8044f816566c59b852efc1d1a65
daltonpurnell <daltonpurnell@live.com>
2015-10-29 14:22:17 -0600

    attempted vimeo but had to switch back to youtube

--- QuickMARSales/QuickMARSales/HomeViewController.h
+++ QuickMARSales/QuickMARSales/HomeViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "YTPlayerView.h"
 
+
 @interface HomeViewController : UIViewController
+
+
 
 @end
--- QuickMARSales/QuickMARSales/HomeViewController.m
+++ QuickMARSales/QuickMARSales/HomeViewController.m
@@ -10,7 +10,7 @@
 
 @interface HomeViewController ()
 
-@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
+//@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
 @property (weak, nonatomic) IBOutlet YTPlayerView *playerView;
 
 @end
@@ -21,13 +21,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    // load video into player view
+    // load youtube video into player view
     [self.playerView loadWithVideoId:@"4LhHBE2RyqY"];
-    
     self.tabBarController.tabBar.barTintColor =  [UIColor colorWithRed:0.533 green:0.764 blue:0.216 alpha:1.000];
     self.tabBarController.tabBar.tintColor = [UIColor whiteColor];
-    
-    
+
 }
 
 
@@ -37,6 +35,8 @@
     [self dismissViewControllerAnimated:YES completion:nil];
     
 }
+
+
 
 /*
 #pragma mark - Navigation
