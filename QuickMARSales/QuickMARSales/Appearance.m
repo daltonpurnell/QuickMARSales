49c7e3393489369d19273e658911c88a7c0ed48b
daltonpurnell <daltonpurnell@live.com>
2015-11-04 10:28:15 -0700

    adds appearance controller

--- QuickMARSales/QuickMARSales/Appearance.m
+++ QuickMARSales/QuickMARSales/Appearance.m
@@ -17,26 +17,25 @@
     
 //    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:136/255.0 green:195/255.0 blue:55/255.0 alpha:1]];
 //    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
-    
-//    [[UITabBar appearance] setBarTintColor: [UIColor colorWithRed:136/255.0 green:195/255.0 blue:55/255.0 alpha:1]];
-    
-    [[UITabBar appearance] setTintColor:[UIColor colorWithRed:74/255.0 green:75/255.0 blue:76/255.0 alpha:1]];
-    
 //    [[UIBarButtonItem appearance] setTintColor:[UIColor whiteColor]];
-    
-    [[UISegmentedControl appearance] setTintColor:[UIColor colorWithRed:136/255.0 green:195/255.0 blue:55/255.0 alpha:1]];
-    
-    [[UILabel appearance] setTextColor:[UIColor colorWithRed:74/255.0 green:75/255.0 blue:76/255.0 alpha:1]];
-    
 //    [[UINavigationBar appearance] setTranslucent:NO];
     
-    [[UITabBar appearance] setTranslucent:NO];
-        
+    
 //    [[UINavigationBar appearance] setTitleTextAttributes: [NSDictionary dictionaryWithObjectsAndKeys:
 //                                                           [UIColor whiteColor],                                                           NSForegroundColorAttributeName,
 //                                                           [UIFont fontWithName:@"SystemFont" size: 22.0],
 //                                                           NSFontAttributeName,
 //                                                           nil]];
+    
+    
+
+//    [[UITabBar appearance] setBarTintColor: [UIColor colorWithRed:136/255.0 green:195/255.0 blue:55/255.0 alpha:1]];
+    [[UITabBar appearance] setTintColor:[UIColor colorWithRed:74/255.0 green:75/255.0 blue:76/255.0 alpha:1]];
+    [[UITabBar appearance] setTranslucent:NO];
+    
+    [[UILabel appearance] setTextColor:[UIColor colorWithRed:74/255.0 green:75/255.0 blue:76/255.0 alpha:1]];
+    
+
     
 }
 
