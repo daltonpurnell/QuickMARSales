d13a733bca7ed7855a296c72fac0b5781830932f
daltonpurnell <daltonpurnell@live.com>
2015-11-11 14:46:55 -0700

    .

--- QuickMARSales/DerivedData/QuickMARSales/Build/Intermediates/QuickMARSales.build/Debug-iphonesimulator/QuickMARSales.build/Objects-normal/x86_64/JGActionSheet.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/djp1989/Documents/Development/QuickMAR/QuickMARSales/QuickMARSales/QuickMARSales/JGActionSheet.m \
-  /Users/djp1989/Documents/Development/QuickMAR/QuickMARSales/QuickMARSales/QuickMARSales/JGActionSheet.h
